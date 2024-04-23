93bc0b625f674d5197098a6d9a999c7933ce661b
Flora Mae Requillo <143053138+floramae2@users.noreply.github.com>
2024-04-22 20:04:05 -0700

    Add files via upload

---
+++ demo/login.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+include "db_conn.php";
+
+if(isset($_POST['uname']) && isset($_POST['password'])) {
+ 
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+$uname = validate($_POST['uname']);
+$pass = validate($_POST['password']);
+
+if(empty($uname)) {
+    header ("Location: index.php?error=User Name is required");
+    exit();
+}
+else if(empty($pass)) {
+    header ("Location: index.php?error=Password is required");
+    exit();
+}
+
+$sql = "SELECT *FROM users WHERE user_name='$uname' AND password='$pass'";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    if($row['user_name'] === $uname && $row['password'] === $pass) {
+        // User authenticated successfully
+        echo "Logged In!";
+        $_SESSION['user_name'] = $row['user_name'];
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['id'] = $row['id'];
+        header("Location: home.php");
+        exit();
+    } else {
+        // Incorrect password
+        header("Location: index.php?error=Incorrect User Name or Password");
+        exit();
+    }
+} else {
+    // No user found with the provided credentials
+    header("Location: index.php?error=Incorrect User Name or Password");
+    exit();
+}
+
+?>
